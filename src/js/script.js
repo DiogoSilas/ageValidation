948a6df7684047ef35596df17420bb11101d51f7
Diogo Silas <diogosilas4@gmail.com>
2023-12-16 15:53:34 -0300

    Modified: Tentando encontrar caminha para imagens do JavaScript

--- src/js/script.js
+++ src/js/script.js
@@ -17,21 +17,21 @@ btnVerificar.addEventListener("click", ()=> {
         if(formSex[0].checked) {
             genero = 'Homem'
             if(idade >= 0 && idade <= 2) {
-                img.setAttribute('src', '../images/foto-bebe.jpg')
+                img.setAttribute('src', './src/images/foto-bebe.jpg')
             } else if(idade >= 3 && idade <= 4) {
-                img.setAttribute('src', '../images/foto-bebe-homem.jpg')
+                img.setAttribute('src', './src/images/foto-bebe-homem.jpg')
             } else if(idade >= 5 && idade <= 8) {
-                img.setAttribute('src', '../images/foto-subcrianca-homem.jpg')
+                img.setAttribute('src', './src/images/foto-subcrianca-homem.jpg')
             } else if(idade >= 9 && idade <= 12) {
-                img.setAttribute('src', '../images/foto-crianca-homem.jpg')
+                img.setAttribute('src', './src/images/foto-crianca-homem.jpg')
             } else if(idade >= 13 && idade <= 17) {
-                img.setAttribute('src','../images/foto-adolecente-homem.jpg' )
+                img.setAttribute('src','./src/images/foto-adolecente-homem.jpg' )
             } else if(idade >= 18 && idade <= 24) {
-                img.setAttribute('src', '../images/foto-jovem-homem.jpg')
+                img.setAttribute('src', './src/images/foto-jovem-homem.jpg')
             } else if(idade >= 25 && idade <= 62) {
-                img.setAttribute('src', '../images/foto-adulto-homem.jpg')
+                img.setAttribute('src', './src/images/foto-adulto-homem.jpg')
             } else if(idade >= 63 && idade <= 140) {
-                img.setAttribute('src', '../images/foto-idoso-homem.jpg')
+                img.setAttribute('src', './src/images/foto-idoso-homem.jpg')
             } else if(idade > 141) {
                 img.setAttribute('class', '')
             }
@@ -39,21 +39,21 @@ btnVerificar.addEventListener("click", ()=> {
         } else if(formSex[1]) {
             genero = 'Mulher'
             if(idade >= 0 && idade <= 2) {
-                img.setAttribute('src', '../images/foto-bebe.jpg')
+                img.setAttribute('src', './src/images/foto-bebe.jpg')
             } else if(idade >= 3 && idade <= 4) {
-                img.setAttribute('src', '../images/foto-bebe-mulher.jpg')
+                img.setAttribute('src', './src/images/foto-bebe-mulher.jpg')
             } else if(idade >= 5 && idade <= 8) {
-                img.setAttribute('src', '../images/foto-subcrianca-mulher.jpg')
+                img.setAttribute('src', './src/images/foto-subcrianca-mulher.jpg')
             } else if(idade >= 9 && idade <= 12) {
-                img.setAttribute('src', '../images/foto-crianca-mulher.jpg')
+                img.setAttribute('src', './src/images/foto-crianca-mulher.jpg')
             } else if(idade >= 13 && idade <= 17) {
-                img.setAttribute('src','../images/foto-adolecente-mulher.jpg' )
+                img.setAttribute('src','./src/images/foto-adolecente-mulher.jpg' )
             } else if(idade >= 18 && idade <= 24) {
-                img.setAttribute('src', '../images/foto-jovem-mulher.jpg')
+                img.setAttribute('src', './src/images/foto-jovem-mulher.jpg')
             } else if(idade >= 25 && idade <= 62) {
-                img.setAttribute('src', '../images/foto-adulto-mulher.jpg')
+                img.setAttribute('src', './src/images/foto-adulto-mulher.jpg')
             } else if(idade >= 63 && idade <= 140) {
-                img.setAttribute('src', '../images/foto-idosa-mulher.jpg')
+                img.setAttribute('src', './src/images/foto-idosa-mulher.jpg')
             } else if(idade >= 141) {
                 img.setAttribute('class', '')
             }
